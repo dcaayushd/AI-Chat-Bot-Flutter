bb332191a6175e4316305bd44ef6f35993d63dc5
Aayush D.C Dangi <aayushdc123@yahoo.com>
2024-06-12 12:31:13 +0545

    API Service Updated

--- lib/apis/api_service.dart
+++ lib/apis/api_service.dart
@@ -1,8 +1,7 @@
 // import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ApiService {
-    // static String apiKey = 'YOUR_OWN_API_KEY';
+    static String apiKey = 'YOUR_OWN_API_KEY';
     // static String apiKey = dotenv.env['API_KEY'] ?? 'API_KEY not found';
-    static String apiKey = 'AIzaSyCmxXLq__RxQGO7OvPkRI8l1GaRLeZn29g';
 
 }
