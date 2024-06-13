f59e9ff5221385ee04e9a4653a3bd1a47ee5308a
Aayush D.C Dangi <aayushdc123@yahoo.com>
2024-06-13 12:03:07 +0545

    Chat Messages Updated

--- lib/widgets/chat_messages.dart
+++ lib/widgets/chat_messages.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-
-import '../models/message.dart';
-import '../providers/chat_provider.dart';
-
-import '../widgets/assistant_message_widget.dart';
-import '../widgets/my_message_widget.dart';
+import 'package:chatbotapp/models/message.dart';
+import 'package:chatbotapp/providers/chat_provider.dart';
+import 'package:chatbotapp/widgets/assistant_message_widget.dart';
+import 'package:chatbotapp/widgets/my_message_widget.dart';
 
 class ChatMessages extends StatelessWidget {
   const ChatMessages({
@@ -22,6 +20,7 @@ class ChatMessages extends StatelessWidget {
       controller: scrollController,
       itemCount: chatProvider.inChatMessages.length,
       itemBuilder: (context, index) {
+        // compare with timeSent before showing the list
         final message = chatProvider.inChatMessages[index];
         return message.role.name == Role.user.name
             ? MyMessageWidget(message: message)
