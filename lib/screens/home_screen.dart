a9b7546bac7b5e04da66022ffc3ae51dd58c13b7
Aayush D.C Dangi <aayushdc123@yahoo.com>
2024-06-14 12:15:50 +0545

    Home Screen Updated

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:chatbotapp/providers/chat_provider.dart';
 import 'package:chatbotapp/screens/chat_history_screen.dart';
 import 'package:chatbotapp/screens/chat_screen.dart';
 import 'package:chatbotapp/screens/profile_screen.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,63 +14,51 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  // Page View Controller
-
-  final PageController _pageController = PageController();
-
-  // ! List of Screens
-
+  // list of screens
   final List<Widget> _screens = [
     const ChatHistoryScreen(),
     const ChatScreen(),
     const ProfileScreen(),
-  ]; // appBar: AppBar(
-  //   backgroundColor: Theme.of(context).colorScheme.primary,
-  //   title: const Text('AI CHAT BOT'),
-  // ),
-
-  // ? Index of current Screen
-
-  int _currentIndex = 0;
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: PageView(
-        controller: _pageController,
-        children: _screens,
-        onPageChanged: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        elevation: 0,
-        selectedItemColor: Theme.of(context).colorScheme.primary,
-        onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-          _pageController.jumpToPage(index);
-        },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.history),
-            // icon: Icon(CupertinoIcons.timelapse),
-            label: 'Chat History',
+    return Consumer<ChatProvider>(
+      builder: (context, chatProvider, child) {
+        return Scaffold(
+          body: PageView(
+            controller: chatProvider.pageController,
+            children: _screens,
+            onPageChanged: (index) {
+              chatProvider.setCurrentIndex(newIndex: index);
+            },
           ),
-          BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.chat_bubble),
-            label: 'Chat',
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: chatProvider.currentIndex,
+            elevation: 0,
+            selectedItemColor: Theme.of(context).colorScheme.primary,
+            onTap: (index) {
+              chatProvider.setCurrentIndex(newIndex: index);
+              chatProvider.pageController.jumpToPage(index);
+            },
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.history),
+                // icon: Icon(CupertinoIcons.timelapse),
+                label: 'Chat History',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(CupertinoIcons.chat_bubble),
+                label: 'Chat',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(CupertinoIcons.person),
+                label: 'Profile',
+              ),
+            ],
           ),
-          BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.person),
-            label: 'Profile',
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
